c3eec6267a81faf2d3dd78a1b97216ddd0a061e8
KiraButter <oliviyazilber@mail.ru>
2020-11-03 15:22:11 +0300

    Доработка

--- js/promo.js
+++ js/promo.js
@@ -2,13 +2,19 @@ var btnBack = document.querySelector(".btn-back");
 var btnNext = document.querySelector(".btn-next");
 var item1 = document.querySelector(".item-1");
 var item2 = document.querySelector(".item-2");
+var btnRadioNext = document.querySelector(".btn-radio-next");
+var btnRadioBack = document.querySelector(".btn-radio-back");
 
 btnNext.addEventListener("click", function() {
     item2.classList.add("show");
     item1.classList.add("none");
+    btnRadioBack.classList.remove("active-btn-radio");
+    btnRadioNext.classList.add("active-btn-radio");
 });
 
 btnBack.addEventListener("click", function() {
     item2.classList.remove("show");
     item1.classList.remove("none");
+    btnRadioNext.classList.remove("active-btn-radio");
+    btnRadioBack.classList.add("active-btn-radio");
 });
